0fa3063e467d8b543188e95e29c2753d06df25e4
Andy Reagan <andy@andyreagan.com>
2015-07-16 11:33:13 -0400

    ready to go

---
+++ README.md
@@ -0,0 +1,3 @@
+Start by launching the webserver with `python -m SimpleHTTPServer` and hack away!
+
+
---
+++ js/my-viz.js
@@ -0,0 +1,6 @@
+var mysvg = d3.select("#myfigure").append("svg");
+
+mysvg.append("rect")
+    .attr("height",600)
+    .attr("width",600)
+    .attr("fill","black");
