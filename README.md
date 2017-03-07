462591bba07019567f99d713dc4ca8a6b208a670
Andy Reagan <andy@andyreagan.com>
2017-03-07 09:04:50 -0500

    updated for v4

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 Start by launching the webserver with `python -m SimpleHTTPServer` and hack away!
 
 
+If you're hip and running `python3`, that would be `python3 -m http.server`.
+Or if you have `node` installed, their `http-server` is more stable under many requests in my experience (but generally, not necessary).
+
--- js/my-viz.js
+++ js/my-viz.js
@@ -1,6 +1,11 @@
-var mysvg = d3.select("#myfigure").append("svg");
+// Shorthand for $( document ).ready()
+$(function() {
+    // let's get started
+    var mysvg = d3.select("#myfigure").append("svg");
 
-mysvg.append("rect")
-    .attr("height",600)
-    .attr("width",600)
-    .attr("fill","black");
+    mysvg.append("rect")
+        .attr("height",600)
+        .attr("width",600)
+        .attr("fill","black");
+});
+
