0cd1e3e7e4fa14e9a965aeef39b898c77dc1b7bb
Andy Reagan <andy@andyreagan.com>
2017-08-08 20:25:15 -0400

    Simple data load example.

--- js/my-viz.js
+++ js/my-viz.js
@@ -7,5 +7,12 @@ $(function() {
         .attr("height",600)
         .attr("width",600)
         .attr("fill","black");
+
+    console.log("Finished drawing rectangle.");
+
+    d3.text("data/my-data.txt",function(e,d) {
+        console.log("Data loaded. Data:");
+        console.log(d);
+    });
 });
 
